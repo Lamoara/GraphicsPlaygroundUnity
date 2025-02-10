e3e33a67b7bf24555f03f04845e1561ef0ee1c55
Lamoara <jorginos40@gmail.com>
2025-02-10 16:34:38 +0100

    init

--- Assets/VoxelRenderer/BinaryVoxels/BinaryChunk.cs
+++ Assets/VoxelRenderer/BinaryVoxels/BinaryChunk.cs
@@ -1,16 +1,49 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BinaryChunk : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int[] voxelMap;
+    int size;
+
+
+    struct Face
+    {
+        int[] vertexIndices;
+        Vector3[] vertex;
+        Vector3[] normals;
+    };
+
+
     void Start()
     {
-        
+        Init();
+        InitRandomVoxels();
+    }
+    void Init()
+    {
+       size = sizeof(int) * 8;
+        voxelMap = new int[size * size];
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitRandomVoxels()
     {
-        
+        for (int i= 0; i < voxelMap.Length; i++)
+        {
+            voxelMap[i] = Random.Range(0, int.MaxValue);
+        }
+    }
+
+    void CreateMesh()
+    {
+        int[][] faces = new int[6][];
+        for (int i = 0; i < faces.Length; i++)
+        {
+            faces[i] = new int[size];
+        }
+
+        List<Face>[] renderFaces = new List<Face>[6];
+
+
     }
 }
