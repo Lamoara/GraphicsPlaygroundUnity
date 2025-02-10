4724d8ce4149d835b7d0d98617441dcd49794fe8
Lamoara <jorginos40@gmail.com>
2025-02-10 14:15:11 +0100

    NextProyectsSetup

---
+++ Assets/VoxelRenderer/BinaryVoxels/BinaryChunk.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BinaryChunk : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
