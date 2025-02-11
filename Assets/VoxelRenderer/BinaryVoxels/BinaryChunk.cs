26beec84ef54e9466cb98860aa79ae2767d8e99e
Lamoara <jorginos40@gmail.com>
2025-02-11 12:39:35 +0100

    Basic meshing calc

--- Assets/VoxelRenderer/BinaryVoxels/BinaryChunk.cs
+++ Assets/VoxelRenderer/BinaryVoxels/BinaryChunk.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Internal.Execution;
 using UnityEngine;
 
 public class BinaryChunk : MonoBehaviour
@@ -10,8 +13,15 @@ public class BinaryChunk : MonoBehaviour
     struct Face
     {
         int[] vertexIndices;
-        Vector3[] vertex;
+        Vector3[] vertices;
         Vector3[] normals;
+
+        public Face(int[] indices, Vector3[] verts, Vector3[] norms)
+        {
+            vertexIndices = indices;
+            vertices = verts;
+            normals = norms;
+        }
     };
 
 
@@ -19,6 +29,7 @@ public class BinaryChunk : MonoBehaviour
     {
         Init();
         InitRandomVoxels();
+        CreateMesh();
     }
     void Init()
     {
@@ -30,13 +41,14 @@ public class BinaryChunk : MonoBehaviour
     {
         for (int i= 0; i < voxelMap.Length; i++)
         {
-            voxelMap[i] = Random.Range(0, int.MaxValue);
+            voxelMap[i] = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
         }
     }
 
     void CreateMesh()
     {
         int[][] faces = new int[6][];
+        //!TODO poner shader para generar caras visibles
         for (int i = 0; i < faces.Length; i++)
         {
             faces[i] = new int[size];
@@ -44,6 +56,54 @@ public class BinaryChunk : MonoBehaviour
 
         List<Face>[] renderFaces = new List<Face>[6];
 
+        //Caras frontales
+
+        int[] meshed = new int[size * size];
+        //!TODO Cambiar esto para usar las caras que deberia generar el shader
+        for (int z = 0; z < size; z++)
+        {
+            for (int x = 0; x < size - 1; x++)
+            {
+                int i = 0;
+                int voxelValue = voxelMap[z * size + x];
+                while (i < size)
+                    {
+                    int mask = 0;
+
+                    // Encuentra el primer bit en 1
+                    while (i < size && ((voxelValue >> i) & 1) == 0) 
+                    {
+                        i++;
+                    }
+                    int height = 1;
+                    // Construye la máscara de los 1s encontrados
+                    while (i < size && ((voxelValue >> i) & 1) == 1) 
+                    {
+                        mask |= 1 << i; // Activa el bit en la posición i
+                        i++;
+                        height ++;
+                    }
+
+                    if (mask == 0) 
+                        break;
+
+                    int width = 1;
+                    while (x + width < size && (meshed[z * size + x + width] & mask) == 0 && (voxelMap[z * size + x + width] & mask) == mask)
+                    {
+                        print("Mesheable");
+                        meshed[z * size + x + width] |= mask;   
+                        width ++;
+                    }
+                    meshed[z * size + x] |= mask;
+
+
+                    //!TODO Construir triangulo con width y height
+                    print(Convert.ToString(voxelValue, 2));
+
+                    print(Convert.ToString(mask, 2));
+                }
+            }
+        }
 
     }
 }
